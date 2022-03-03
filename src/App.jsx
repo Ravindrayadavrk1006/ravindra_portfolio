0c8e9c3a165ababf23ca72b78496d62959c33c1d
ravindrayadavrk1006 <ravindrayadavrk1006@gmail.com>
2022-03-03 12:28:18 +0530

    initial setup of components and styling

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav';
+import About from './components/about/About';
+import Skills from './components/skills/Skills';
+import Testimonials from './components/testimonials/Testimonials';
+import Projects from './components/projects/Projects';
+import Footer from './components/footer/Footer';
+import Contact from './components/contact/Contact';
+function App() {
+  return (
+    <>
+      <Header/>
+      <Nav></Nav>
+      <About></About>
+      <Skills></Skills>
+      <Projects></Projects>
+      <Testimonials/>
+      <Contact></Contact>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
